92bd4be1d2ce270b147c52d3023a11f095036db0
education-bootcamp <education.seekerscloud@gmail.com>
2025-08-17 11:27:56 +0530

    6

--- src/app/pages/home-page/inner-items/stays-form-component/stays-form-component.ts
+++ src/app/pages/home-page/inner-items/stays-form-component/stays-form-component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {
+  MatDatepickerModule,
+  MatDatepickerToggle,
+  MatDateRangeInput,
+  MatDateRangePicker
+} from '@angular/material/datepicker';
+import {MatFormField, MatLabel, MatSuffix} from '@angular/material/input';
+import {MatFormFieldModule, MatHint} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-stays-form-component',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatIcon,
+    MatFormField,
+    MatLabel,
+    MatDateRangeInput,
+    MatHint,
+    MatSuffix,
+    MatDatepickerToggle,
+    MatDateRangePicker
+  ],
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './stays-form-component.html',
   styleUrl: './stays-form-component.scss'
 })
