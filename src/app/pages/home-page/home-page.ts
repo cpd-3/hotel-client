3260f61b1539e44c6f97edec9be78ec1e9936f3a
education-bootcamp <education.seekerscloud@gmail.com>
2025-08-17 12:31:40 +0530

    10

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MainHeader} from './inner-items/main-header/main-header';
 import {OptionsComponent} from './inner-items/options-component/options-component';
+import {OffersComponent} from './inner-items/offers-component/offers-component';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     MainHeader,
-    OptionsComponent
+    OptionsComponent,
+    OffersComponent
   ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
