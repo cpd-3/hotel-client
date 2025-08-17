69a2c6c2279e1b67b6cfb692e23dd091295ec34f
education-bootcamp <education.seekerscloud@gmail.com>
2025-08-17 11:01:24 +0530

    5

--- src/app/pages/home-page/inner-items/options-component/options-component.ts
+++ src/app/pages/home-page/inner-items/options-component/options-component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatTab, MatTabGroup, MatTabLabel} from '@angular/material/tabs';
 import {MatIcon} from '@angular/material/icon';
+import {StaysContextComponent} from '../stays-context-component/stays-context-component';
 
 @Component({
   selector: 'app-options-component',
@@ -8,7 +9,8 @@ import {MatIcon} from '@angular/material/icon';
     MatTabGroup,
     MatTab,
     MatTabLabel,
-    MatIcon
+    MatIcon,
+    StaysContextComponent
   ],
   templateUrl: './options-component.html',
   styleUrl: './options-component.scss'
---
+++ src/app/pages/home-page/inner-items/stays-form-component/stays-form-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stays-form-component',
+  imports: [],
+  templateUrl: './stays-form-component.html',
+  styleUrl: './stays-form-component.scss'
+})
+export class StaysFormComponent {
+
+}
